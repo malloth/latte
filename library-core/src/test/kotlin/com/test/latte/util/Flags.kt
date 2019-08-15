ba72bb8b3bc9be53dd86654edce5d6d15d429ebb
malloth <malloth@orange.pl>
2019-08-15 13:39:44 +0200

    Added unit tests

---
+++ library-core/src/test/kotlin/com/test/latte/util/Flags.kt
@@ -0,0 +1,28 @@
+package com.test.latte.util
+
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class Flags {
+
+    @Test
+    fun `returns true when contains a given flag`() {
+        val flags = 0b00001011
+        val flag  = 0b00000010
+
+        val result = flags.hasFlags(flag)
+
+        assertTrue(result)
+    }
+
+    @Test
+    fun `returns false when does not contain a given flag`() {
+        val flags = 0b00001011
+        val flag  = 0b00000100
+
+        val result = flags.hasFlags(flag)
+
+        assertFalse(result)
+    }
+}
