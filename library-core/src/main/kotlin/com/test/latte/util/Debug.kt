42809c8af0687e70cc35156685c7aeeec600517f
malloth <malloth@orange.pl>
2019-08-15 11:38:32 +0200

    Refactored method for getting a debug id

--- library-core/src/main/kotlin/com/test/latte/util/Debug.kt
+++ library-core/src/main/kotlin/com/test/latte/util/Debug.kt
@@ -3,21 +3,30 @@ package com.test.latte.util
 import android.view.View
 import android.view.View.NO_ID
 
-internal val View.debugId: String
+internal val View.debugId: String?
     get() {
         return if (id != NO_ID) {
             if (id > 0 && resourceHasPackage(id) && resources != null) {
-                val pkgName = when (id and -0x1000000) {
-                    0x7f000000 -> "app"
-                    0x01000000 -> "android"
+                val pkgName = when (id and RES_OFFSET_MASK) {
+                    RES_OFFSET_APP -> PACKAGE_APP
+                    RES_OFFSET_ANDROID -> PACKAGE_ANDROID
                     else -> resources.getResourcePackageName(id)
                 }
                 val typeName = resources.getResourceTypeName(id)
                 val entryName = resources.getResourceEntryName(id)
 
                 "$pkgName:$typeName/$entryName"
-            } else "#${Integer.toHexString(id)}"
-        } else "null"
+            } else "#${id.toHexString()}"
+        } else null
     }
 
 private fun resourceHasPackage(resId: Int): Boolean = resId.ushr(24) != 0
+
+private fun Int.toHexString(): String = Integer.toHexString(this)
+
+private const val PACKAGE_ANDROID = "android"
+private const val PACKAGE_APP = "app"
+
+private const val RES_OFFSET_ANDROID = 0x01000000
+private const val RES_OFFSET_APP = 0x7f000000
+private const val RES_OFFSET_MASK = -0x1000000
