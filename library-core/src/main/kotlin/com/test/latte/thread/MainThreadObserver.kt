458483f75236563cd6a18a4fc51909df3381dbe3
malloth <malloth@orange.pl>
2019-08-18 16:58:40 +0200

    Replaced test runner dependency with test monitor

--- library-core/src/main/kotlin/com/test/latte/thread/MainThreadObserver.kt
+++ library-core/src/main/kotlin/com/test/latte/thread/MainThreadObserver.kt
@@ -1,11 +1,11 @@
 package com.test.latte.thread
 
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
 
 @PublishedApi
 internal object MainThreadObserver : ThreadObserver {
 
-    private val instrumentation = InstrumentationRegistry.getInstrumentation()
+    private val instrumentation = getInstrumentation()
 
     override fun waitUntilIdle() = instrumentation.waitForIdleSync()
 }
